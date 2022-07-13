ccfb84d7db7a0708ef5aef48b100ae8649a059d5
bastrodriguezr <bast.rodriguezr@gmail.com>
2022-07-13 15:35:05 -0400

    Added apps settings to initialize website

--- K_Pro_Petstore/settings.py
+++ K_Pro_Petstore/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'core',
+    'crud',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-CL'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'AMERICA/Santiago'
 
 USE_I18N = True
 
--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
+
+def root(request):
+    return redirect('/home')
+
+def home(request):
+    return render(request, 'core/home.html')
