885c73de04dd58d0d6625ab5dea8792e7a3aef70
bastrodriguezr <bast.rodriguezr@gmail.com>
2022-07-14 18:06:13 -0400

    Added Table Marca

---
+++ crud/forms.py
@@ -0,0 +1,4 @@
+from dataclasses import fields
+from django import forms
+from django.forms import ModelForm, widgets
+from .models import *
--- crud/models.py
+++ crud/models.py
@@ -4,8 +4,16 @@ from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
-# class Marca(models.Model):
-#     marca 
+class Marca(models.Model):
+    marca = models.CharField(max_length=50, verbose_name='Marca')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-#     def __str__(self):
-#         return self
+    class Meta():
+        verbose_name = 'marca'
+        verbose_name_plural = 'marcas'
+        ordering = ['marca']
+
+    def __str__(self):
+        return self.marca
+
