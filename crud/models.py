dcb29b8e6718e90f2e5d4969b77e7d2b94bf105c
bastrodriguezr <bast.rodriguezr@gmail.com>
2022-07-14 17:36:06 -0400

    Added settings changes

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,11 @@
+from distutils.command.upload import upload
+from hashlib import blake2b
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+# class Marca(models.Model):
+#     marca 
+
+#     def __str__(self):
+#         return self
