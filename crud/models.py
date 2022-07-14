dab60094be259afa4353bfab15cf68a169f6f85b
bastrodriguezr <bast.rodriguezr@gmail.com>
2022-07-14 18:18:41 -0400

    Added Table Producto

--- crud/models.py
+++ crud/models.py
@@ -6,10 +6,11 @@ from django.db import models
 # Create your models here.
 class Marca(models.Model):
     marca = models.CharField(max_length=50, verbose_name='Marca')
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    class Meta():
+    class Meta:
         verbose_name = 'marca'
         verbose_name_plural = 'marcas'
         ordering = ['marca']
@@ -17,3 +18,22 @@ class Marca(models.Model):
     def __str__(self):
         return self.marca
 
+class Producto(models.Model):
+    id_producto = models.CharField(primary_key=True,max_length=20, verbose_name='ID')
+    nombre = models.CharField(max_length=50, verbose_name='Nombre')
+    precio = models.IntegerField(verbose_name='Precio')
+    stock = models.IntegerField(verbose_name='Stock')
+    imagen = models.ImageField(verbose_name='Imagen', upload_to='productos',null=True, blank=True)
+    descripcion = models.CharField(max_length=200, verbose_name='Descripción')
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE, null=True, blank=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name='producto'
+        verbose_name_plural='productos'
+        ordering=['id_producto']
+
+    def __str__(self):
+        return self
