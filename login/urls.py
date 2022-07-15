c0acf9613f13004f98644005b1177e6d04ff93af
bastrodriguezr <bast.rodriguezr@gmail.com>
2022-07-15 11:53:44 -0400

    Added links fixed

--- login/urls.py
+++ login/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home, name='home'),
     path('register', register, name='register'),
     path('login', login, name='login'),
     path('logout', logout, name='logout')
